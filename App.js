a35520640a96f9a45b9a819b5d6a995f186ad9a3
Utkarsh Khanna <utkarshkhannaaisn@gmail.com>
2023-07-08 10:10:09 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1 className="title">NutritionCrafted</h1>
+        <p className="tagline">Fuel Your Journey to a Healthier You!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
